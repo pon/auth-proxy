084c24a8cb7ea444554d58c4b0d1531f2ff8447f
Peter Nagel <pnagel5@gmail.com>
2014-06-30 22:33:44 -0700

    moved db stuff to its own file

--- test/unit/db.js
+++ test/unit/db.js
@@ -1,29 +1,29 @@
 var Lab       = require('lab');
-var auth      = require('../../lib/auth');
+var db        = require('../../lib/db');
 var fs        = require('fs');
 var sqlite3   = require('sqlite3');
 
 Lab.experiment('checkDbExists', function () {
 
-  var db = __dirname + '/../../data/existsTest.sqlite3';
+  var dbPath = __dirname + '/../../data/existsTest.sqlite3';
 
   Lab.before(function (done) {
-    fs.openSync(db, 'w');
+    fs.openSync(dbPath, 'w');
     done();
   });
 
   Lab.test('returns false when db does not exist', function (done) {
-    Lab.expect(auth.checkDbExists('asdf')).to.equal(false);
+    Lab.expect(db.checkDbExists('asdf')).to.equal(false);
     done();
   });
 
   Lab.test('returns true when db does exist', function (done) {
-    Lab.expect(auth.checkDbExists('existsTest')).to.equal(true);
+    Lab.expect(db.checkDbExists('existsTest')).to.equal(true);
     done();
   });
 
   Lab.after(function (done) {
-    fs.unlinkSync(db);
+    fs.unlinkSync(dbPath);
     done();
   });
 });
@@ -40,11 +40,11 @@ Lab.experiment('createEmptyDb', function () {
   });
 
   Lab.test('should create the empty db', function (done) {
-    auth.createEmptyDb('createTest', function (err) {
+    db.createEmptyDb('createTest', function (err) {
       Lab.expect(err).to.equal(null);
-      var db = new sqlite3.Database(dbPath, function (err) {
+      var createDb = new sqlite3.Database(dbPath, function (err) {
         Lab.expect(err).to.equal(null);
-        db.get('SELECT name FROM sqlite_master ' +
+        createDb.get('SELECT name FROM sqlite_master ' +
                'WHERE type = \'table\' AND name = \'users\'', function (err, row) {
           Lab.expect(err).to.equal(null);
           Lab.expect(row.name).to.equal('users');
